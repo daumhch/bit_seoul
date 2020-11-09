5fed4d2bc67901e53cccc67ccf27000eb15a258c
daumhch <66451256+daumhch@users.noreply.github.com>
2020-11-09 20:56:33 +0900

    acc가 도저히 올라가지 않는다

--- homework/hcbae01.py
+++ homework/hcbae01.py
@@ -5,13 +5,15 @@ import numpy as np
 
 x = np.array([1,2,3,4,5,6])
 y = np.array([1,2,3,4,5,6])
-x2 = np.array([11,12,13,14,15,16])
+x2 = np.array([11,12,13,14])
 
 
 model = Sequential()
-model.add(Dense(10, input_dim=1))
-model.add(Dense(100))
-model.add(Dense(40))
+model.add(Dense(32, input_dim=1))
+model.add(Dense(512))
+model.add(Dense(512))
+model.add(Dense(512))
+model.add(Dense(64))
 model.add(Dense(1))
 
 
@@ -23,9 +25,9 @@ metrics=['acc',
 'categorical_accuracy'])
 
 
-model.fit(x, y, epochs=100)
+model.fit(x, y, epochs=100, batch_size=1, validation_split=0.2, verbose=0)
 
-loss, acc, accuracy, categorical_accuracy = model.evaluate(x, y)
+loss, acc, accuracy, categorical_accuracy = model.evaluate(x, y, batch_size=1)
 print("loss : ", loss)
 print("acc : ", acc)
 print("accuracy : ", accuracy)
