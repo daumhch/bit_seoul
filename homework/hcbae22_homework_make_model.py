b40c5a853ac53944d0def89a93211d70d1eb1af6
daumhch <66451256+daumhch@users.noreply.github.com>
2020-11-20 14:41:26 +0900

    일단 된다

--- homework/hcbae22_homework_make_model.py
+++ homework/hcbae22_homework_make_model.py
@@ -23,25 +23,25 @@ def split_x2(seq, size):
 # 1. 데이터
 # 1.1 load_data
 import numpy as np
-samsung_data = np.load('./data/samsung_data.npy', allow_pickle=True)
+samsung_data = np.load('./data/samsung_data.npy', allow_pickle=True).astype('float32')
 samsung_data = split_x2(samsung_data, view_size)
 samsung_data = samsung_data[:-1]
 # print("type(samsung_data):",type(samsung_data))
 # print("samsung_data:",samsung_data)
 # print("samsung_data.shape:", samsung_data.shape)
-samsung_target = np.load('./data/samsung_target.npy', allow_pickle=True)
+samsung_target = np.load('./data/samsung_target.npy', allow_pickle=True).astype('float32')
 samsung_target = samsung_target[view_size:]
 # print("samsung_target:",samsung_target)
 # print('samsung_target.shape',samsung_target.shape)
 
 
-bitcom_data = np.load('./data/bitcom_data.npy', allow_pickle=True)
+bitcom_data = np.load('./data/bitcom_data.npy', allow_pickle=True).astype('float32')
 bitcom_data = split_x2(bitcom_data, view_size)
 bitcom_data = bitcom_data[:-1]
 # print("type(bitcom_data):",type(bitcom_data))
 # print("bitcom_data:",bitcom_data)
 # print("bitcom_data.shape:", bitcom_data.shape)
-bitcom_target = np.load('./data/bitcom_target.npy', allow_pickle=True)
+bitcom_target = np.load('./data/bitcom_target.npy', allow_pickle=True).astype('float32')
 bitcom_target = bitcom_target[view_size:]
 # print("bitcom_target:",bitcom_target)
 # print('bitcom_target.shape',bitcom_target.shape)
@@ -117,8 +117,13 @@ print("after scaled bitcom_data_test.shape:",bitcom_data_test.shape)
 
 
 # 1.4 reshape
+samsung_data_train = samsung_data_train.reshape(samsung_data_train.shape[0],samsung_data_train.shape[1]*samsung_data_train.shape[2])
+samsung_data_test = samsung_data_test.reshape(samsung_data_test.shape[0],samsung_data_test.shape[1]*samsung_data_test.shape[2])
+print("after reshape x:", samsung_data_train.shape, samsung_data_test.shape)
 
-
+bitcom_data_train = bitcom_data_train.reshape(bitcom_data_train.shape[0],bitcom_data_train.shape[1]*bitcom_data_train.shape[2])
+bitcom_data_test = bitcom_data_test.reshape(bitcom_data_test.shape[0],bitcom_data_test.shape[1]*bitcom_data_test.shape[2])
+print("after reshape x:", bitcom_data_train.shape, bitcom_data_test.shape)
 
 
 # modelpath = './model/hcbae22_{epoch:02d}_{val_loss:.4f}.hdf5'
@@ -127,57 +132,103 @@ print("after scaled bitcom_data_test.shape:",bitcom_data_test.shape)
 
 # 2.모델
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Input
 
-samsung_model = Sequential()
-samsung_model.add(Dense(30, name='samsung_model_1', activation='relu', input_shape=(samsung_data_train.shape[1],) ))
-samsung_model.add(Dense(30, name='samsung_model_2', activation='relu') )
-samsung_model.add(Dense(20, name='samsung_model_3', activation='relu') )
-samsung_model.add(Dense(1, name='samsung_model_4', activation='relu') )
-samsung_model.summary()
+samsung_model_input = Input(shape=(samsung_data_train.shape[1],))
+dense1 = Dense(20, activation='relu', name='dense1_1')(samsung_model_input)
+dense1 = Dense(10, activation='relu', name='dense1_2')(dense1)
+dense1 = Dense(5, activation='relu', name='dense1_3')(dense1)
+samsung_model_output1 = Dense(1, name='samsung_model_output1')(dense1)
 
-bitcom_model = Sequential()
-bitcom_model.add(Dense(30, name='bitcom_model_1', activation='relu', input_shape=(bitcom_data_train.shape[1],) ))
-bitcom_model.add(Dense(30, name='bitcom_model_2', activation='relu') )
-bitcom_model.add(Dense(20, name='bitcom_model_3', activation='relu') )
-bitcom_model.add(Dense(1, name='bitcom_model_4', activation='relu') )
-bitcom_model.summary()
+bitcom_model_input = Input(shape=(bitcom_data_train.shape[1],))
+dense2 = Dense(20, activation='relu', name='dense2_1')(bitcom_model_input)
+dense2 = Dense(10, activation='relu', name='dense2_2')(dense2)
+dense2 = Dense(5, activation='relu', name='dense2_3')(dense2)
+bitcom_model_output1 = Dense(1, name='bitcom_model_output1')(dense2)
 
-from tensorflow.keras.layers import Concatenate, concatenate
-merge1 = Concatenate(axis=1)([samsung_model.output, bitcom_model.output])
+
+from tensorflow.keras.layers import concatenate
+merge1 = concatenate([samsung_model_output1, bitcom_model_output1])
 middle = Dense(30, name='middle1')(merge1)
 middle = Dense(7, name='middle2')(middle)
 middle = Dense(11, name='middle3')(middle)
 
 samsung_out = Dense(30, name='output1_1')(middle)
 samsung_out = Dense(7, name='output1_2')(samsung_out)
-samsung_out = Dense(1, name='output1_3')(samsung_out)
-
-bitcom_out = Dense(15, name='output2_0')(middle)
-bitcom_out = Dense(17, name='output2_1')(bitcom_out)
-bitcom_out = Dense(11, name='output2_3')(bitcom_out)
-bitcom_out = Dense(1, name='output2_4')(bitcom_out)
+samsung_model_output2 = Dense(1, name='output1_3')(samsung_out)
 
 
-total_model = Model(inputs=[samsung_model.input,bitcom_model.input], 
-                    outputs=[samsung_out,bitcom_out])
+total_model = Model(inputs=[samsung_model_input,bitcom_model_input], 
+                    outputs=samsung_model_output2)
 total_model.summary()
 
 
 
-# samsung_data_train = scaling3D(samsung_data_train, samsung_scaler)
-# samsung_data_test = transform3D(samsung_data_test, samsung_scaler)
-# bitcom_data_train = scaling3D(bitcom_data_train, bitcom_scaler)
-# bitcom_data_test
+modelpath = './model/hcbae22_{epoch:02d}_{val_loss:.4f}.hdf5'
+model_save_path = "./save/hcbae22_model.h5"
+weights_save_path = './save/hcbae22_weights.h5'
+
 # 3. 컴파일, 훈련
-total_model.compile( # 컴파일
-    loss='mse', # 오차함수는 mean squared error를 사용한다
-    optimizer='adam', # 최적화 방법은 'adam'을 사용한다
-    metrics=['mae']) # mean absolute error 지표를 추가한다
+total_model.compile(
+    loss='mse',
+    optimizer='adam',
+    metrics=['mae'])
+
+from tensorflow.keras.callbacks import EarlyStopping # 조기 종료
+early_stopping = EarlyStopping(
+    monitor='val_loss',
+    patience=50,
+    mode='auto',
+    verbose=2)
+
+from tensorflow.keras.callbacks import ModelCheckpoint # 모델 체크 포인트
+model_check_point = ModelCheckpoint(
+    filepath=modelpath,
+    monitor='val_loss',
+    save_best_only=True,
+    mode='auto')
 
 #훈련, 일단 x_train, y_train 입력하고
 total_model.fit([samsung_data_train, bitcom_data_train], 
-    epochs=256, # 훈련 횟수
-    batch_size=32, # 훈련 데이터단위
+    samsung_target_train,
+    epochs=1000, # 훈련 횟수
+    batch_size=128, # 훈련 데이터단위
     validation_split=0.2,
     verbose=1)
+
+total_model.save(model_save_path)
+total_model.save_weights(weights_save_path)
+
+
+# 4. 평가, 예측
+result = total_model.evaluate([samsung_data_test, bitcom_data_test], 
+                                samsung_target_test, batch_size=128)
+print("loss: ", result[0])
+print("mae: ", result[1])
+
+y_predict = total_model.predict([samsung_data_test, bitcom_data_test])
+print("y_predict:", y_predict)
+
+
+
+y_recovery = samsung_target_test
+
+# 사용자정의 RMSE 함수
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+
+print("RMSE:", RMSE(y_recovery, y_predict))
+
+
+# 사용자정의 R2 함수
+# 사이킷런의 metrics에서 r2_score를 불러온다
+from sklearn.metrics import r2_score
+r2 = r2_score(y_recovery, y_predict)
+print("R2:", r2)
+
+
+
+
+
+
