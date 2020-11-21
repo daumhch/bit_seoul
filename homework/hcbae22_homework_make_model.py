24a609b3a7b35e744b1669ad60db8d898dab6824
daumhch <66451256+daumhch@users.noreply.github.com>
2020-11-21 15:13:13 +0900

    samsung_conv2d

--- homework/hcbae22_homework_make_model.py
+++ homework/hcbae22_homework_make_model.py
@@ -2,8 +2,9 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # 디버그 메시지 끄기
 
 
-view_size = 25
-
+view_size = 3
+before_days = 3
+test_rate = 0.2
 def split_x2(seq, size):
     bbb = []
     for i in range(len(seq) - size + 1):
@@ -25,45 +26,45 @@ def split_x2(seq, size):
 import numpy as np
 samsung_data = np.load('./data/samsung_data.npy', allow_pickle=True).astype('float32')
 samsung_data = split_x2(samsung_data, view_size)
-samsung_pred = samsung_data[-3]
+samsung_pred = samsung_data[-(1+before_days)]
 # print("samsung_pred:\n",samsung_pred)
 
-samsung_data = samsung_data[:-2]
+samsung_data = samsung_data[:-before_days]
 # print("type(samsung_data):",type(samsung_data))
 # print("samsung_data:\n",samsung_data)
 # print("samsung_data.shape:", samsung_data.shape)
 samsung_target = np.load('./data/samsung_target.npy', allow_pickle=True).astype('float32')
-samsung_target = samsung_target[view_size+1:]
+samsung_target = samsung_target[view_size+before_days-1:]
 # print("samsung_target:\n",samsung_target)
 # print('samsung_target.shape',samsung_target.shape)
 
 
 bitcom_data = np.load('./data/bitcom_data.npy', allow_pickle=True).astype('float32')
 bitcom_data = split_x2(bitcom_data, view_size)
-bitcom_pred = bitcom_data[-1]
+bitcom_pred = bitcom_data[-(1+before_days)]
 # print("bitcom_pred:\n",bitcom_pred)
 
-bitcom_data = bitcom_data[:-2]
+bitcom_data = bitcom_data[:-before_days]
 # print("type(bitcom_data):",type(bitcom_data))
 # print("bitcom_data:",bitcom_data)
 # print("bitcom_data.shape:", bitcom_data.shape)
 bitcom_target = np.load('./data/bitcom_target.npy', allow_pickle=True).astype('float32')
-bitcom_target = bitcom_target[view_size+1:]
+bitcom_target = bitcom_target[view_size+before_days-1:]
 # print("bitcom_target:",bitcom_target)
 # print('bitcom_target.shape',bitcom_target.shape)
 
 
 gold_data = np.load('./data/gold_data.npy', allow_pickle=True).astype('float32')
 gold_data = split_x2(gold_data, view_size)
-gold_pred = gold_data[-1]
+gold_pred = gold_data[-(1+before_days)]
 # print("gold_pred:\n",gold_pred)
 
-gold_data = gold_data[:-2]
+gold_data = gold_data[:-before_days]
 # print("type(gold_data):",type(gold_data))
 # print("gold_data:",gold_data)
 # print("gold_data.shape:", gold_data.shape)
 gold_target = np.load('./data/gold_target.npy', allow_pickle=True).astype('float32')
-gold_target = gold_target[view_size+1:]
+gold_target = gold_target[view_size+before_days-1:]
 # print("gold_target:",gold_target)
 # print('gold_target.shape',gold_target.shape)
 
@@ -71,15 +72,15 @@ gold_target = gold_target[view_size+1:]
 
 kosdaq_data = np.load('./data/kosdaq_data.npy', allow_pickle=True).astype('float32')
 kosdaq_data = split_x2(kosdaq_data, view_size)
-kosdaq_pred = kosdaq_data[-1]
+kosdaq_pred = kosdaq_data[-(1+before_days)]
 # print("kosdaq_pred:\n",kosdaq_pred)
 
-kosdaq_data = kosdaq_data[:-2]
+kosdaq_data = kosdaq_data[:-before_days]
 # print("type(kosdaq_data):",type(kosdaq_data))
 # print("kosdaq_data:",kosdaq_data)
 # print("kosdaq_data.shape:", kosdaq_data.shape)
 kosdaq_target = np.load('./data/kosdaq_target.npy', allow_pickle=True).astype('float32')
-kosdaq_target = kosdaq_target[view_size+1:]
+kosdaq_target = kosdaq_target[view_size+before_days-1:]
 # print("kosdaq_target:",kosdaq_target)
 # print('kosdaq_target.shape',kosdaq_target.shape)
 
@@ -105,28 +106,28 @@ print('kosdaq_target.shape',kosdaq_target.shape)
 # 1.2 train_test_split
 from sklearn.model_selection import train_test_split 
 samsung_data_train,samsung_data_test, samsung_target_train,samsung_target_test = train_test_split(
-    samsung_data, samsung_target, train_size=0.9, test_size=0.1, random_state = 44)
+    samsung_data, samsung_target, train_size=(1.-test_rate), test_size=test_rate, random_state = 44)
 # print("after samsung_data_train.shape:\n",samsung_data_train.shape)
 # print("after samsung_data_test.shape:\n",samsung_data_test.shape)
 # print("samsung_data_train[0]:\n",samsung_data_train[0])
 # print("samsung_data_test[0]:\n",samsung_data_test[0])
 
 bitcom_data_train,bitcom_data_test, bitcom_target_train,bitcom_target_test = train_test_split(
-    bitcom_data, bitcom_target, train_size=0.9, test_size=0.1, random_state = 44)
+    bitcom_data, bitcom_target, train_size=(1.-test_rate), test_size=test_rate, random_state = 44)
 # print("after bitcom_data_train.shape:\n",bitcom_data_train.shape)
 # print("after bitcom_data_test.shape:\n",bitcom_data_test.shape)
 # print("bitcom_data_train[0]:\n",bitcom_data_train[0])
 # print("bitcom_data_test[0]:\n",bitcom_data_test[0])
 
 gold_data_train,gold_data_test, gold_target_train,gold_target_test = train_test_split(
-    gold_data, gold_target, train_size=0.9, test_size=0.1, random_state = 44)
+    gold_data, gold_target, train_size=(1.-test_rate), test_size=test_rate, random_state = 44)
 # print("after gold_data_train.shape:\n",gold_data_train.shape)
 # print("after gold_data_test.shape:\n",gold_data_test.shape)
 # print("gold_data_train[0]:\n",gold_data_train[0])
 # print("gold_data_test[0]:\n",gold_data_test[0])
 
 kosdaq_data_train,kosdaq_data_test, kosdaq_target_train,kosdaq_target_test = train_test_split(
-    kosdaq_data, kosdaq_target, train_size=0.9, test_size=0.1, random_state = 44)
+    kosdaq_data, kosdaq_target, train_size=(1.-test_rate), test_size=test_rate, random_state = 44)
 # print("after kosdaq_data_train.shape:\n",kosdaq_data_train.shape)
 # print("after kosdaq_data_test.shape:\n",kosdaq_data_test.shape)
 # print("kosdaq_data_train[0]:\n",kosdaq_data_train[0])
@@ -232,46 +233,48 @@ remove_file('./model')
 # 2.모델
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.layers import Conv2D, Flatten
+from tensorflow.keras.layers import Conv2D, Flatten, GRU
 from tensorflow.keras.layers import MaxPooling2D, Dropout
 
 def custom_model(model, node):
-    temp = Conv2D(node, (3,3), padding='same', activation='relu')(model)
-    temp = Conv2D(node/2, (1,1), padding='valid', activation='relu')(temp)
+    temp = Conv2D(node, (2,2), padding='same', activation='relu')(model)
+    temp = Conv2D(node, (1,1), padding='same', activation='relu')(temp)
+    temp = MaxPooling2D(pool_size=(2,2),strides=1)(temp)
     temp = Dropout(0.2)(temp)
-    # temp = Conv2D(node*2, (3,3), padding='same', activation='relu')(temp)
-    # temp = Conv2D(node, (1,1), padding='valid', activation='relu')(temp)
-    # temp = Dropout(0.3)(temp)
-    # temp = Conv2D(node*4, (3,3), padding='same', activation='relu')(temp)
-    # temp = Conv2D(node*2, (1,1), padding='valid', activation='relu')(temp)
-    # temp = Dropout(0.4)(temp)
-    # temp = Conv2D(node*8, (3,3), padding='same', activation='relu')(temp)
-    # temp = Conv2D(node*4, (1,1), padding='valid', activation='relu')(temp)
-    # temp = Dropout(0.5)(temp)
-    temp = Flatten()(temp) 
+    temp = Conv2D(node*2, (2,2), padding='same', activation='relu')(temp)
+    temp = Conv2D(node*2, (1,1), padding='same', activation='relu')(temp)
+    temp = MaxPooling2D(pool_size=(2,2),strides=1)(temp)
+    temp = Dropout(0.3)(temp)
+    temp = Flatten()(temp)
     return temp
 
 samsung_model_input = Input(shape=(samsung_data_train.shape[1],samsung_data_train.shape[2],1))
-samsung_model_output1 = custom_model(samsung_model_input, 64)
+samsung_model_output1 = custom_model(samsung_model_input, 512)
 
 bitcom_model_input = Input(shape=(bitcom_data_train.shape[1],bitcom_data_train.shape[2],1))
-bitcom_model_output1 = custom_model(bitcom_model_input, 4)
+bitcom_model_output1 = custom_model(bitcom_model_input, 64)
 
 gold_model_input = Input(shape=(gold_data_train.shape[1],gold_data_train.shape[2],1))
-gold_model_output1 = custom_model(gold_model_input, 4)
+gold_model_output1 = custom_model(gold_model_input, 64)
 
 kosdaq_model_input = Input(shape=(kosdaq_data_train.shape[1],kosdaq_data_train.shape[2],1))
-kosdaq_model_output1 = custom_model(kosdaq_model_input, 4)
+kosdaq_model_output1 = custom_model(kosdaq_model_input, 64)
 
 
 
 from tensorflow.keras.layers import concatenate
 samsung_out = concatenate([samsung_model_output1, bitcom_model_output1,
                        gold_model_output1, kosdaq_model_output1])
-samsung_out = Dense(768, activation='relu')(samsung_out)
+# samsung_out = Dense(1024, activation='relu')(samsung_out)
+# samsung_out = Dense(1024, activation='relu')(samsung_out)
 samsung_out = Dense(512, activation='relu')(samsung_out)
-samsung_out = Dense(256, activation='relu')(samsung_out)
-samsung_out = Dense(128, activation='relu')(samsung_out)
+samsung_out = Dense(512, activation='relu')(samsung_out)
+# samsung_out = Dense(256, activation='relu')(samsung_out)
+# samsung_out = Dense(256, activation='relu')(samsung_out)
+# samsung_out = Dense(128, activation='relu')(samsung_out)
+# samsung_out = Dense(128, activation='relu')(samsung_out)
+# samsung_out = Dense(64, activation='relu')(samsung_out)
+# samsung_out = Dense(64, activation='relu')(samsung_out)
 samsung_model_output2 = Dense(1, name='output1_3')(samsung_out)
 
 
@@ -308,7 +311,7 @@ hist = total_model.fit([samsung_data_train, bitcom_data_train,
                         epochs=1000, # 훈련 횟수
                         batch_size=512, # 훈련 데이터단위
                         verbose=1,
-                        validation_split=0.2,
+                        validation_split=0.25,
                         callbacks=[early_stopping,
                         model_check_point
                         ])
@@ -334,9 +337,11 @@ y_predict = total_model.predict([samsung_data_test, bitcom_data_test,
 
 
 y_recovery = samsung_target_test
+print("y_test:", y_recovery)
+print("y_predict:", y_predict)
 
 # 사용자정의 RMSE 함수
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_squared_log_error
 def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
 
@@ -348,32 +353,36 @@ print("RMSE:", RMSE(y_recovery, y_predict))
 from sklearn.metrics import r2_score
 r2 = r2_score(y_recovery, y_predict)
 print("R2:", r2)
+print("loss: ", result[0])
+print("mae: ", result[1])
 
+
+print("RMSLE:",np.sqrt(mean_squared_log_error(y_recovery, y_predict)))
 
 
 # predict 11~17일 데이터
 # samsung_predict_today 19일 시가 (실제 "64,100")
-print("samsung_pred:\n",samsung_pred)
-print("samsung_pred.shape:",samsung_pred.shape)
+# print("samsung_pred:\n",samsung_pred)
+# print("samsung_pred.shape:",samsung_pred.shape)
 samsung_pred = samsung_pred.reshape(1, samsung_pred.shape[0],samsung_pred.shape[1])
 samsung_pred = transform3D(samsung_pred, samsung_scaler)
-#samsung_pred = samsung_pred.reshape(1, samsung_pred.shape[0]*samsung_pred.shape[1])
-print("samsung_pred.shape:",samsung_pred.shape)
+# samsung_pred = samsung_pred.reshape(1, samsung_pred.shape[0]*samsung_pred.shape[1])
+# print("samsung_pred.shape:",samsung_pred.shape)
 
 bitcom_pred = bitcom_pred.reshape(1, bitcom_pred.shape[0],bitcom_pred.shape[1])
 bitcom_pred = transform3D(bitcom_pred, bitcom_scaler)
-#bitcom_pred = bitcom_pred.reshape(1, bitcom_pred.shape[0]*bitcom_pred.shape[1])
-print("bitcom_pred.shape:",bitcom_pred.shape)
+# bitcom_pred = bitcom_pred.reshape(1, bitcom_pred.shape[0]*bitcom_pred.shape[1])
+# print("bitcom_pred.shape:",bitcom_pred.shape)
 
 gold_pred = gold_pred.reshape(1, gold_pred.shape[0],gold_pred.shape[1])
 gold_pred = transform3D(gold_pred, gold_scaler)
-#gold_pred = gold_pred.reshape(1, gold_pred.shape[0]*gold_pred.shape[1])
-print("gold_pred.shape:",gold_pred.shape)
+# gold_pred = gold_pred.reshape(1, gold_pred.shape[0]*gold_pred.shape[1])
+# print("gold_pred.shape:",gold_pred.shape)
 
 kosdaq_pred = kosdaq_pred.reshape(1, kosdaq_pred.shape[0],kosdaq_pred.shape[1])
 kosdaq_pred = transform3D(kosdaq_pred, kosdaq_scaler)
-#kosdaq_pred = kosdaq_pred.reshape(1, kosdaq_pred.shape[0]*kosdaq_pred.shape[1])
-print("kosdaq_pred.shape:",kosdaq_pred.shape)
+# kosdaq_pred = kosdaq_pred.reshape(1, kosdaq_pred.shape[0]*kosdaq_pred.shape[1])
+# print("kosdaq_pred.shape:",kosdaq_pred.shape)
 
 samsung_predict_today = total_model.predict([samsung_pred, bitcom_pred,
                                             gold_pred, kosdaq_pred])
@@ -393,6 +402,7 @@ plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
 plt.grid()
 plt.title('loss')
 plt.ylabel('loss')
+plt.yscale('log')
 plt.xlabel('epochs')
 plt.legend(loc='upper right')
 
@@ -402,6 +412,7 @@ plt.plot(hist.history['val_mae'], marker='.', c='blue')
 plt.grid()
 plt.title('mae')
 plt.ylabel('mae')
+plt.yscale('log')
 plt.xlabel('epochs')
 plt.legend(['mae', 'val_mae'])
 
