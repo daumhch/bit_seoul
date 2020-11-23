8e8ef2543e552ab9fde71d5a2f6f5d217e4ecb13
daumhch <66451256+daumhch@users.noreply.github.com>
2020-11-23 20:44:17 +0900

    마지막 백업

--- ml/m11_selelctModel2.py
+++ ml/m11_selelctModel2.py
@@ -9,16 +9,16 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-iris = pd.read_csv('./data/csv/boston_house_prices.csv', 
+boston = pd.read_csv('./data/csv/boston_house_prices.csv', 
                         header=0, # 첫 번 째 행 = 헤더다
                         index_col=0, # 컬럼 번호
                         encoding='CP949',
                         sep=',' # 구분 기호
                         )
-iris = iris[1:]
-print(iris)
-x = iris.iloc[:,:-1]
-y = iris.iloc[:,-1:]
+boston = boston[1:]
+print(boston)
+x = boston.iloc[:,:-1]
+y = boston.iloc[:,-1:]
 
 # 1.2 train_test_split
 from sklearn.model_selection import train_test_split 
@@ -38,6 +38,18 @@ for (name, algorithm) in allAlgorithms:
         pass
 
 # 원래 50여개의 모델이 나와야 하는데, 버전바뀌고 에러가 발생한다
+
+
+
+
+
+from sklearn.ensemble import GradientBoostingRegressor
+model2 = GradientBoostingRegressor()
+model2.fit(x_train, y_train)
+y_pred2 = model2.predict(x_test)
+print('GradientBoostingRegressor:', r2_score(y_test, y_pred2))
+
+
 
 
 
