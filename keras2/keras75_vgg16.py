361c9d1765382626e277aa835855c184e7b4d38b
daumhch <66451256+daumhch@users.noreply.github.com>
2020-12-04 10:25:52 +0900

    오전 백업

---
+++ keras2/keras75_vgg16.py
@@ -0,0 +1,38 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # 디버그 메시지 끄기
+
+
+
+# 전이학습 = 다른 사람이 만든거, 가중치까지 빼서 가져다 쓴다
+
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
+
+# model = VGG16() # parameter = 138,357,544
+# model = VGG16(weights='imagenet') # parameter = 138,357,544
+
+# input_shape를 바꾸려면, include_tope을 false로 놓고, input_shape를 입력한다
+vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=(32,32,3) ) # 14,714,688
+
+# 새로 훈련시킬꺼야
+# model.trainable = True # Trainable params: 14,714,688
+
+# 훈련 시키지 않고, imagenet train을 사용할꺼야
+vgg16.trainable = False # Non-trainable params: 14,714,688
+
+vgg16.summary()
+
+print('동결하기 전 훈련되는 가중치의 수')
+print(':', len(vgg16.trainable_weights)) 
+# VGG16() 모델 그대로 만들면 32 = 16개의 w, 16개의 b
+# trainable = False 라면, 가중치 0
+
+
+# 모델 출력부분 붙이기
+model = Sequential()
+model.add(vgg16)
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+model.summary()
+
