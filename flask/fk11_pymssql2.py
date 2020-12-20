d691aae362ece87766a5b2c6af0ff303934b3c6d
daumhch <daumhch@naver.com>
2020-12-21 00:18:36 +0900

    pymssql 실습해봄

---
+++ flask/fk11_pymssql2.py
@@ -0,0 +1,15 @@
+import pymssql as ms
+
+conn = ms.connect(server='127.0.0.1', user='bit2', password='1234', database='bitdb')
+
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM ADDRESS;')
+
+row = cursor.fetchone()
+
+while row:
+    print("첫컬럼=%s, 둘컬럼=%s" %(row[0], row[1]))
+    row = cursor.fetchone()
+
+conn.close()
